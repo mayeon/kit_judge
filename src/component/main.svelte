b9fffde63e381872d53c2b2f59d4a9cdc475f0af
Junjae Lee <ejj612o@gmail.com>
2022-04-05 22:50:05 +0900

    라우터 디렉토리 분리

--- src/component/main.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    import { link } from "svelte-spa-router";
-</script>
-
-<h1>main</h1>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
-import Main from "./component/main.svelte";
-import UserRegister from "./component/userRegister.svelte";
-import Login from "./component/login.svelte";
+import UserRegister from "./routes/userRegister.svelte";
+import Login from "./routes/login.svelte";
 
 const routes = {
     "/": Login,
-    "/main/": Main,
     "/login": Login,
     "/user-register": UserRegister,
 };
