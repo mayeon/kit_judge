7de3fafde539caf4ec6fffde1123bdc5f6fb67f0
Junjae Lee <ejj612o@gmail.com>
2022-04-07 22:30:51 +0900

    과목 블록추가

---
+++ src/component/block.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let name;
+    export let class_name;
+    export let explain;
+    export let d_day;
+    export let is_star;
+</script>
+
+<div>
+    <h1>과제이름 {name}</h1>
+    <h2>과목명 {class_name}</h2>
+    <h2>설명 {explain}</h2>
+    <h2>디데이 {d_day}</h2>
+
+    {#if is_star}
+        ⭐
+    {:else}
+        ☆
+    {/if}
+</div>
+
+<style>
+    div {
+        border: 1px black solid;
+    }
+</style>
