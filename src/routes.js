a769b2f0c6c15e6ac214c0cde202f2a60c9fd6cc
Junjae Lee <ejj612o@gmail.com>
2022-04-07 22:31:06 +0900

    라우터 추가

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import UserRegister from "./routes/userRegister.svelte";
 import Login from "./routes/login.svelte";
+import Main from "./routes/main.svelte";
 
 const routes = {
     "/": Login,
     "/login": Login,
     "/user-register": UserRegister,
+    "/main": Main,
 };
 
 export default routes
