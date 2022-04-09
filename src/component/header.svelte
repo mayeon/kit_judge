7a2703585700a4896c4766c7daba6aa176bdba52
Mayeon <bmg5539@naver.com>
2022-04-09 20:29:13 +0900

    css적용

--- src/component/header.svelte
+++ src/component/header.svelte
@@ -5,32 +5,79 @@
     let options = [
         { id: 1, text: `강의실 1` },
         { id: 2, text: `강의실 2` },
-        { id: 3, text: `강의실 2` },
+        { id: 3, text: `강의실 3` },
     ];
+
+    // 로그인 여부 (임시)
+
+    let user = {loggedIn:true};
+    // let user = {loggedIn:false};
 </script>
 
-<header>
-    <p>금오공대 과제 채점 시스템</p>
-    <a href="/" use:link>Home</a>
-    <form>
-        <select bind:value={selected}>
-            {#each options as option}
-                <option value={option}>
-                    {option.text}
-                </option>
-            {/each}
-        </select>
-    </form>
-</header>
+<div class="container">
+    <div class="row">
+        <div class="col col-lg-4"></div>
+        <div class="col col-lg-2">
+            <div class="home-btn">
+                <button type="button" class="btn btn-outline-secondary btn-light" onclick="location.href='/'">HOME</button>
+            </div>
+        </div>
+
+        <div class="col col-lg-2">
+            {#if user.loggedIn}
+                <div class="dropdown">
+                    <button type="button" class="btn btn-outline-secondary btn-light dropdown-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    강의실 선택
+                    </button>
+            
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        {#each options as option}
+                            <li><a class="dropdown-item" href="/">{option.text}</a></li>
+                        {/each}
+                    </ul>
+                </div>
+            {/if}
+
+        </div>
+        <div class="col col-lg-4"></div>
+    </div>
+</div>
 
 <style>
-    header {
-        background-color: gray;
+    .container {
         margin: 0;
-        display: flex;
+        padding: 0;
+        max-width: 1920px;
+        height: 4rem;
+        background-color: rgb(155, 155, 155);
+        text-align: center;
     }
 
-    p {
-        margin: 0;
+    .row {
+        width: 100%
+    }
+
+    .container .col-lg-2 div{
+        line-height: 4rem;
+    }
+
+    .dropdown-menu {
+        line-height: 2rem;
+    }
+
+    .dropdown-item {
+        color: rgb(123, 131, 138);
+    }
+
+    .dropdown-item:hover, .dropdown-item:focus {
+        color: #1e2125;
+        background-color: #e9ecef;
+        text-decoration: none;
+    }
+
+    .dropdown-item:active {
+        color: rgb(255, 255, 255);
+        background-color: rgb(123, 131, 138);
+        text-decoration: none;
     }
 </style>
--- src/routes/userRegister.svelte
+++ src/routes/userRegister.svelte
@@ -12,70 +12,105 @@
     import { passwordMatch, containNumbers } from "./customValidators";
 
     const form = useForm();
-    const requiredMessage = "This field is required";
+    const requiredMessage = "필수 기입 항목입니다.";
 
     let type = false;
 </script>
 
 <main>
-    <form use:form>
+    <form use:form id="register">
         <h1>회원가입</h1>
-        <label for="type">교수자</label>
-        <input type="checkbox" name="type" bind:checked={type} />
+
+        <div class="form-check form-switch switch">
+            <input class="form-check-input" type="checkbox" role="switch" name="type" bind:checked={type}>
+            <label class="form-check-label" for="type">교수자</label>
+        </div>
 
         {#if !type}
-            <label for="studentNum">학번</label>
-            <input
-                type="number"
-                name="studentNum"
-                use:validators={[required, minLength(8)]}
-            />
+            <div class="input-box">
+                <input
+                    type="number"
+                    class="form-control"
+                    name="studentNum"
+                    use:validators={[required, minLength(8)]}
+                    placeholder="학번"
+                />
+                <div class="hint">
+                    <HintGroup for="studentNum">
+                        <Hint on="required">{requiredMessage}</Hint>
+                        <Hint on="minLength" hideWhenRequired let:value>
+                            학번은 {value}자리 입니다.
+                        </Hint>
+                    </HintGroup>
+                </div>
+            </div>
         {/if}
 
-        <label for="id">아이디</label>
-        <input type="text" name="id" use:validators={[required]} />
-        <HintGroup for="id">
-            <Hint on="required">{requiredMessage}</Hint>
-        </HintGroup>
+        <div class="input-box">
+            <input type="text" class="form-control" name="id" use:validators={[required]} placeholder="아이디" />
+            <div class="hint">
+                <HintGroup for="id">
+                    <Hint on="required">{requiredMessage}</Hint>
+                </HintGroup>
+            </div>
+        </div>
 
-        <label for="name">이름</label>
-        <input type="text" name="name" use:validators={[required]} />
+        <div class="input-box">
+            <input type="text" class="form-control" name="name" use:validators={[required]} placeholder="이름" />
+            <div class="hint">
+                <HintGroup for="name">
+                    <Hint on="required">{requiredMessage}</Hint>
+                </HintGroup>
+            </div>
+        </div>
 
-        <label for="password">비밀번호</label>
-        <input
-            type="password"
-            name="password"
-            use:validators={[required, minLength(5), containNumbers(2)]}
-        />
-        <HintGroup for="password">
-            <Hint on="required">{requiredMessage}</Hint>
-            <Hint on="minLength" hideWhenRequired let:value
-                >This field must have at least {value} characters.</Hint
-            >
-            <Hint on="containNumbers" hideWhen="minLength" let:value>
-                This field must contain at least {value} numbers.
-            </Hint>
-        </HintGroup>
+        <div class="input-box">    
+            <input
+                type="password"
+                class="form-control"
+                name="password"
+                use:validators={[required, minLength(5), containNumbers(2)]}
+                placeholder="비밀번호"
+            />
+            <div class="hint">
+                <HintGroup for="password">
+                    <Hint on="required">{requiredMessage}</Hint>
+                    <Hint on="minLength" hideWhenRequired let:value>
+                        최소 {value}자 이상이여야 합니다.
+                    </Hint>
+                    <Hint on="containNumbers" hideWhen="minLength" let:value> 
+                        최소 {value}개의 숫자가 필요합니다.
+                    </Hint>
+                </HintGroup>
+            </div>
+        </div>
 
-        <label for="passwordConfirmation">비밀번호 확인</label>
-        <input
-            type="password"
-            name="passwordConfirmation"
-            use:validators={[required, passwordMatch]}
-        />
-        <HintGroup for="passwordConfirmation">
-            <Hint on="required">{requiredMessage}</Hint>
-            <Hint on="passwordMatch" hideWhenRequired
-                >Passwords do not match</Hint
-            >
-        </HintGroup><br />
-
-        <button disabled={!$form.valid} on:click|preventDefault> 제출 </button>
+        <div class="input-box">
+            <input
+                type="password"
+                class="form-control"
+                name="passwordConfirmation"
+                use:validators={[required, passwordMatch]}
+                placeholder="비밀번호 확인"
+            />
+            <div class="hint">
+                <HintGroup for="passwordConfirmation">
+                    <Hint on="required">{requiredMessage}</Hint>
+                    <Hint on="passwordMatch" hideWhenRequired>
+                        Passwords do not match
+                        </Hint>
+                </HintGroup>
+            </div>
+        </div>
+        <br>
     </form>
-    <pre>
+    <button form="register" id="register-btn" class="btn btn-outline-secondary" disabled={!$form.valid} on:click|preventDefault> 제출 </button>
+
+
+    <!-- <pre>
 		{JSON.stringify($form, null, 1)}
         교수자 : {type}
-	</pre>
+	</pre> -->
 </main>
 
 <style>
@@ -85,13 +120,47 @@
     }
 
     main {
-        display: flex;
-        justify-content: space-around;
+        display: block;
+        text-align: center;
+        width: 100%;
+        max-width: 15rem;
+        padding: 1rem;
+        margin: auto;
+        margin-top: 3rem;
     }
 
-    pre {
+    .switch {
+        float: right;
+        padding: 0;
+    }
+
+    #register-btn {
+        float: right;
+        margin-top: -1rem;
+    }
+
+    #register h1 {
+        padding-bottom: 1rem;
+    }
+
+    #register .input-box {
+        margin-bottom: 2rem;
+    }
+
+    #register input {
+        margin-bottom: -1px;
+    }
+
+    .hint {
+        position: absolute;
+        height: 1.5rem;
+        padding-left: 0.5rem;
+        color: red;
+    }
+
+    /* pre {
         height: 80vh;
         overflow: auto;
         font-size: 12px;
-    }
+    } */
 </style>
