0aff376769997298fa3b1c4565456e8e7688737b
Junjae Lee <ejj612o@gmail.com>
2022-04-14 22:22:13 +0900

    에디터 추가

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,16 @@ import Login from "./routes/login.svelte";
 import Main from "./routes/main.svelte";
 import codeWrite from "./routes/codeEditor.svelte";
 import Test from "./routes/test.svelte";
+import AssigmentWrite from "./routes/assigmentEditor.svelte";
 
 const routes = {
     "/": Login,
     "/login": Login,
     "/user-register": UserRegister,
     "/main": Main,
-    "/write": codeWrite,
+    "/write-code": codeWrite,
     "/test": Test,
+    "/write-assigment": AssigmentWrite,
 };
 
 export default routes
---
+++ src/routes/assigmentEditor.svelte
@@ -0,0 +1,55 @@
+<script>
+    import { onMount } from "svelte";
+    import Quill from "quill";
+    let quill = null;
+
+    onMount(() => {
+        let container = document.getElementById("editor");
+        quill = new Quill(container, {
+            modules: {
+                toolbar: [
+                    [
+                        { header: 1 },
+                        { header: 2 },
+                        "blockquote",
+                        "link",
+                        "image",
+                        "video",
+                    ],
+                    ["bold", "italic", "underline", "strike"],
+                    [{ list: "ordered" }, { list: "ordered" }],
+                    [{ align: [] }],
+                    ["clean"],
+                ],
+            },
+            placeholder: "Type something...",
+            theme: "snow", // or 'bubble'
+        });
+    });
+</script>
+
+<svelte:head>
+    <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />
+</svelte:head>
+<main>
+    <div id="editor" />
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    #editor {
+        height: 350px;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
