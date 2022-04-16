322744db2a6edbe7622e1d890450975c5e940201
Junjae Lee <ejj612o@gmail.com>
2022-04-16 15:21:51 +0900

    과제 제출페이지 추가

--- src/routes/assigmentEditor.svelte
+++ src/routes/assigmentEditor.svelte
@@ -1,5 +1,9 @@
 <script>
     import { onMount } from "svelte";
+    import Textfield from "@smui/textfield";
+    import HelperText from "@smui/textfield/helper-text";
+    import Button, { Group, Label } from "@smui/button";
+
     import Quill from "quill";
     let quill = null;
 
@@ -26,14 +30,33 @@
             theme: "snow", // or 'bubble'
         });
     });
+
+    let title = "";
+    let className = "";
+
+    import { DateInput } from "date-picker-svelte";
+    let date = new Date();
+    let date1 = new Date();
 </script>
 
 <svelte:head>
     <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />
 </svelte:head>
+
+<Textfield bind:value={title} label="제목">
+    <HelperText slot="helper">Helper Text</HelperText>
+</Textfield>
+<Textfield bind:value={className} label="과목명">
+    <HelperText slot="helper">Helper Text</HelperText>
+</Textfield>
+<h1>시작일</h1>
+<DateInput bind:value={date} format="yyyy-MM-dd HH:mm" />
+<h1>종료일</h1>
+<DateInput bind:value={date1} format="yyyy-MM-dd HH:mm" />
 <main>
     <div id="editor" />
 </main>
+<Button variant="raised" class="button-shaped-round">제출</Button>
 
 <style>
     main {
