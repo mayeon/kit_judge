ce04a27ddd6c48cc74e2b181516d3fbc442f44c5
Junjae Lee <ejj612o@gmail.com>
2022-04-17 17:46:01 +0900

    마진 조금 추가

--- src/component/block.svelte
+++ src/component/block.svelte
@@ -19,18 +19,25 @@
         rel="stylesheet"
     />
 </svelte:head>
+<div class="paper">
+    <Paper>
+        <Title>과제이름 {name}</Title>
+        <IconButton
+            on:click={() => toggleClicked++}
+            toggle
+            bind:pressed={initialOff}
+        >
+            <Icon class="material-icons" on>star</Icon>
+            <Icon class="material-icons">star_border</Icon>
+        </IconButton>
+        <Subtitle>과목명 {class_name}</Subtitle>
+        <Content>설명 {explain}</Content>
+        <Subtitle>디데이 {d_day}</Subtitle>
+    </Paper>
+</div>
 
-<Paper>
-    <Title>과제이름 {name}</Title>
-    <IconButton
-        on:click={() => toggleClicked++}
-        toggle
-        bind:pressed={initialOff}
-    >
-        <Icon class="material-icons" on>star</Icon>
-        <Icon class="material-icons">star_border</Icon>
-    </IconButton>
-    <Subtitle>과목명 {class_name}</Subtitle>
-    <Content>설명 {explain}</Content>
-    <Subtitle>디데이 {d_day}</Subtitle>
-</Paper>
+<style>
+    .paper {
+        margin: 10px;
+    }
+</style>
--- src/routes/main.svelte
+++ src/routes/main.svelte
@@ -5,5 +5,4 @@
 <h1>진행중인과제</h1>
 
 <Block name="연결리스트" />
-
 <Block is_star="true" />
