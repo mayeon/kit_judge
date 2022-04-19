4ace029c4f350225053d0665f79aeb2c4ad8de64
Junjae Lee <ejj612o@gmail.com>
2022-04-19 23:40:13 +0900

    쓸모없는거 제거

--- src/component/header.svelte
+++ src/component/header.svelte
@@ -1,27 +1,13 @@
 <script>
     import { push } from "svelte-spa-router";
     import { link } from "svelte-spa-router";
-    import IconButton, { Icon } from "@smui/icon-button";
     import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
 
-    let selected;
-    let options = [
-        { id: 1, text: `강의실 1` },
-        { id: 2, text: `강의실 2` },
-        { id: 3, text: `강의실 2` },
-    ];
-
-    let prominent = false;
-    let dense = true;
-    let secondaryColor = false;
+    const prominent = false;
+    const dense = true;
 </script>
 
-<TopAppBar
-    variant="static"
-    {prominent}
-    {dense}
-    color={secondaryColor ? "secondary" : "primary"}
->
+<TopAppBar variant="static" {prominent} {dense}>
     <Row>
         <Section>
             <Title on:click={() => push("/")}>금오공대 과제 채점 시스템</Title>
