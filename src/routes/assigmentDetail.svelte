c459cb0144ae19c3c1734e1277836c83706fbf31
Junjae Lee <ejj612o@gmail.com>
2022-04-24 18:55:56 +0900

    과제 정보페이지게 테스트 케이스 추가

--- src/routes/assigmentDetail.svelte
+++ src/routes/assigmentDetail.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-    import Paper, { Title, Subtitle, Content } from "@smui/paper";
+    import Paper, { Title, Subtitle } from "@smui/paper";
+    import Card, { Content } from "@smui/card";
+
     const elevation = 10;
+
+    let testcases = [
+        { input: "12321", output: "12321" },
+        { input: "12321", output: "12321" },
+    ];
 </script>
 
 <div class="paper-container">
@@ -9,6 +16,23 @@
         <Subtitle>과목 이름</Subtitle>
         <Subtitle>기간 : 2020/12/12 ~ 2102/21/12</Subtitle>
         <Content>설명</Content>
+
+        {#each testcases as testcase}
+            <div class="card">
+                <Card>
+                    <div class="content-box">
+                        <Content>
+                            <h2>입력</h2>
+                            {testcase.input}
+                        </Content>
+                        <Content>
+                            <h2>출력</h2>
+                            {testcase.output}
+                        </Content>
+                    </div>
+                </Card>
+            </div>
+        {/each}
     </Paper>
 </div>
 
