9f2f4e7ea849e8896c2b4209c2bafb325c219eb4
Junjae Lee <ejj612o@gmail.com>
2022-04-24 19:26:57 +0900

    테스트 케이스 컴포넌트 추가

---
+++ src/component/Testcase.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Card, { Content } from "@smui/card";
+    import Paper, { Title, Subtitle } from "@smui/paper";
+
+    export let input;
+    export let output;
+</script>
+
+<Card>
+    <div class="content-box">
+        <Subtitle>입력</Subtitle>
+        <Content>
+            {input}
+        </Content>
+        <Subtitle>출력</Subtitle>
+        <Content>
+            {output}
+        </Content>
+    </div>
+    <slot />
+</Card>
