a85e33ce83f3f0d3534944ba6218e36f0793a1e7
Junjae Lee <ejj612o@gmail.com>
2022-04-26 19:34:44 +0900

    [ADD] 제출정보 조회

---
+++ src/routes/assigmentResult.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Paper, { Title, Subtitle } from "@smui/paper";
+    import Card, { Content } from "@smui/card";
+
+    const elevation = 10;
+</script>
+
+<Paper {elevation}>제출 번호, 점수, 코드보기</Paper>
+
+<style>
+</style>
