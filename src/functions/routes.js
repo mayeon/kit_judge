a7d71204ead4bc2a9c60c2dbcdbc41fabd1127d6
Junjae Lee <ejj612o@gmail.com>
2022-05-05 22:37:46 +0900

    유저 페이지 추가

--- src/functions/routes.js
+++ src/functions/routes.js
@@ -2,6 +2,7 @@ import Login from "../routes/login.svelte";
 import Main from "../routes/main.svelte";
 import Test from "../routes/test.svelte";
 import UserRegister from "../routes/userRegister.svelte";
+import UserDetail from "../routes/UserDetail.svelte";
 
 import AssigmentWrite from "../routes/assigmentEditor.svelte";
 import AssigmentDetail from "../routes/assigmentDetail.svelte";
@@ -19,16 +20,21 @@ import Links from "../routes/pageLinks.svelte";
 const routes = {
     "/": Login,
     "/login": Login,
+    "/user": UserDetail,
     "/user/new": UserRegister,
+
     "/main": Main,
     "/code": CodeWrite,
+
     "/assigment": AssigmentDetail,
     "/assigment/new": AssigmentWrite,
     "/assigment/submit": AssigmentResult,
+
     "/class": ClassControll,
     "/class/new": ClassNew,
     "/class/join": ClassJoin,
     "/class/student": ClassStudentControll,
+
     "/test": Test,
     "/links": Links,
 };
---
+++ src/routes/userDetail.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Paper, { Title, Subtitle } from "@smui/paper";
+    const elevation = 10;
+</script>
+
+<div class="paper-container">
+    <Paper {elevation}>
+        <Title>회원정보</Title>
+    </Paper>
+</div>
+
+<style>
+</style>
