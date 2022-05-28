e9414f4f97757a74095dc1793e12817c93f2f10e
Mayeon <bmg5539@naver.com>
2022-05-28 13:19:43 +0900

    axios 파일 업로드 테스트

--- src/routes/uploadTest.svelte
+++ src/routes/uploadTest.svelte
@@ -1,11 +1,23 @@
 <script>
-    import { axiosPost } from "../functions/restAPI";
-    import IconButton from "@smui/icon-button";
+    import axios from "axios";
+    import Button from "@smui/button";
 
     const url = "http://localhost:3000/testDB";
+    
+    async function axiosPost(url, data, header = {"content-type": "application/json; charset=UTF-8",}) {
+        try {
+            return await axios.post(url, data, header);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     function upload() {
-        let file = document.getElementsByName('img')[0].files[0];
+        let file = document.getElementById('img-data').files[0];
+
+        let preview = document.getElementById("img-preview");
+        preview.src = URL.createObjectURL(file);
+        console.log(preview.src);
 
         let formData = new FormData();
         formData.append("uploadFile", file, file.name);
@@ -17,20 +29,22 @@
         axiosPost(url, {data: formData}, config).then((res) => console.log(res));
     }
 
-    function upload2() {
-        let text = document.getElementsByName('text').value;
-        console.log(text)
-        
+    function uploadText() {
+        let text = document.getElementById('text-data').value;
         axiosPost(url, {data: text}).then((res) => console.log(res));
     }
 </script>
 
 <div>
-    <input type="file" name="img" />
+    <input type="file" name="img" id="img-data" />
     <br>
-    <IconButton class="test-button" on:click={upload}>전송</IconButton>
+    <Button class="test-button" on:click={upload}>전송</Button>
+
     <br>
-    <input name="text" /><br>
+
+    <input type="text" name="text" id="text-data"/>
     <br>
-    <IconButton class="test-button" on:click={upload2}>전송</IconButton>
+    <Button class="test-button" on:click={uploadText}>전송</Button>
 </div>
+
+<img id="img-preview" />
