e8d9fc1a9bb9bf8298f094488b92a9c0d8ef73b1
Junjae Lee <ejj612o@gmail.com>
2022-06-03 20:55:05 +0900

    서버변경 로컬 -> 실제

--- src/functions/source.js
+++ src/functions/source.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const protocol = "http";
 const domain = "localhost";
 const port = "5000"
-const sourceURL = protocol + "://" + domain + ":" + port;
+const sourceURL = "https://kumohcheck.d-o-g.fun";
 
 const axiosInstance = axios.create({
     baseURL: sourceURL,
