7031a79e489faa289dec02818a17f2861faf3e46
Junjae Lee <ejj612o@gmail.com>
2022-06-06 01:22:22 +0900

    학생목록 조회

--- src/routes/classStudentControll.svelte
+++ src/routes/classStudentControll.svelte
@@ -7,60 +7,32 @@
         PrimaryText,
         SecondaryText,
     } from "@smui/list";
+    import { beforeUpdate, onMount } from "svelte";
+    import { axiosInstance, sourceURL } from "../functions/source";
 
-    let options = [
-        {
-            name: "Bruce Willis",
-            description: "20190909",
-            disabled: false,
-        },
-        {
-            name: "Austin Powers",
-            description: "20190909",
-            disabled: true,
-        },
-        {
-            name: "Thomas Edison",
-            description: "20190909",
-            disabled: false,
-        },
-    ];
+    export let params = {};
+
+    let students = [];
+
+    onMount(async () => {
+        await axiosInstance
+            .get(`${sourceURL}/classroom/${params.classId}/member`)
+            .then((response) => {
+                students = response.data;
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    });
+
+    let options = [];
+
     let selection = "Stephen Hawking";
     let selectionIndex = "";
 </script>
 
 <div class="container">
-    <div id="request-participation">
-        <h1>참여 희망 학생</h1>
-        <List
-            class="demo-list"
-            twoLine
-            avatarList
-            singleSelection
-            bind:selectedIndex={selectionIndex}
-        >
-            {#each options as item}
-                <Item
-                    on:SMUI:action={() => (selection = item.name)}
-                    disabled={item.disabled}
-                    selected={selection === item.name}
-                >
-                    <Graphic
-                        style="background-image: url(https://place-hold.it/40x40?text={item.name
-                            .split(' ')
-                            .map((val) => val.substring(0, 1))
-                            .join('')}&fontsize=16);"
-                    />
-                    <Text>
-                        <PrimaryText>{item.name}</PrimaryText>
-                        <SecondaryText>{item.description}</SecondaryText>
-                    </Text>
-                    <Meta class="material-icons">add</Meta>
-                </Item>
-            {/each}
-        </List>
-    </div>
-    
     <div id="participated-student">
         <h1>참여 학생</h1>
         <List
@@ -70,21 +42,21 @@
             singleSelection
             bind:selectedIndex={selectionIndex}
         >
-            {#each options as item}
+            {#each students as student}
                 <Item
-                    on:SMUI:action={() => (selection = item.name)}
-                    disabled={item.disabled}
-                    selected={selection === item.name}
+                    on:SMUI:action={() => (selection = student.name)}
+                    disabled={student.disabled}
+                    selected={selection === student.id}
                 >
                     <Graphic
-                        style="background-image: url(https://place-hold.it/40x40?text={item.name
+                        style="background-image: url(https://place-hold.it/40x40?text={student.name
                             .split(' ')
                             .map((val) => val.substring(0, 1))
                             .join('')}&fontsize=16);"
                     />
                     <Text>
-                        <PrimaryText>{item.name}</PrimaryText>
-                        <SecondaryText>{item.description}</SecondaryText>
+                        <PrimaryText>{student.name}</PrimaryText>
+                        <SecondaryText>학번{student.student_id}</SecondaryText>
                     </Text>
                     <Meta class="material-icons">info</Meta>
                 </Item>
@@ -94,7 +66,6 @@
 </div>
 
 <!-- <pre class="status">Selected: {selection}, value of selectedIndex: {selectionIndex}</pre> -->
-
 <style>
     /* * :global(.demo-list) {
         max-width: 600px;
@@ -103,12 +74,18 @@
     .container {
         --element-participation-container-width: 500px;
         --element-participation-container-margin: 20px;
-        width: calc((var(--element-participation-container-width) + var(--element-participation-container-margin) * 2) * 2);
+        width: calc(
+            (
+                    var(--element-participation-container-width) +
+                        var(--element-participation-container-margin) * 2
+                ) * 2
+        );
         position: relative;
         top: 2rem;
     }
 
-    #request-participation, #participated-student {
+    #request-participation,
+    #participated-student {
         display: inline-block;
         margin: calc(var(--element-participation-container-margin) / 2);
         width: var(--element-participation-container-width);
